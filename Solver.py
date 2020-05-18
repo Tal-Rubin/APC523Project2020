ca9c334ee4a3a10c72be51a408c1de0959a44828
Tal-Rubin <62976744+Tal-Rubin@users.noreply.github.com>
2020-05-18 17:49:58 -0400

    Add files via upload

--- Solver.py
+++ Solver.py
@@ -24,11 +24,28 @@ def unpickle(file):
         res = pickle.load(fo, encoding='bytes')
     return res        
         
-def projectDGintoFE(rho,NodeList,GlobalElementMatrix,Proj,DGorder):
+def projectDGintoFE(rho,NodeList,GlobalElementMatrix,DGorder):
     rhoFE=np.zeros(NodeList.shape[1])
+    Proj=np.zeros((9,DGorder))
+    for i in range(9):
+        for j in range(DGorder):
+            Proj[i,j]=QBF.Mano[j]*(QBF.Na[i]*QBF.Ma[j]).intx(-1,1).inty(-1,1).scalar()    
     for k in range(GlobalElementMatrix.shape[0]):
         rhoFE[GlobalElementMatrix[k,1:10]]+=Proj@rho[DGorder*k:DGorder*(k+1)]
     return rhoFE
+
+def projectFEintoDG(rhoFE,NodeList,GlobalElementMatrix,DGorder):
+    rho=np.zeros(GlobalElementMatrix.shape[0]*DGorder)
+    Proj=np.zeros((DGorder,9))
+    for i in range(DGorder):
+        for j in range(9):
+            Proj[i,j]=QBF.Mano[i]*(QBF.Ma[i]*QBF.Na[j]).intx(-1,1).inty(-1,1).scalar()    
+    for k in range(GlobalElementMatrix.shape[0]):
+        rho[DGorder*k:DGorder*(k+1)]+=Proj@rhoFE[GlobalElementMatrix[k,1:10]]
+    return rho
+
+
+
 
 def plotDG(rho,fignum,DGorder):
     Xnode=np.array([])
@@ -85,12 +102,12 @@ def setInitDenDist(x0,y0,r0,denval,rho,NodeList,GlobalElementMatrix,DGorder):
         #rho[9*k]+=1
     return rho,plt.Circle((x0,y0), r0, color='r')
 
+
 def setInitDenDistFE(x0,y0,r0,denval,rho,NodeList,GlobalElementMatrix):
     for k in range(NodeList.shape[1]):
         dist=(NodeList[1,k]-x0)**2+(NodeList[2,k]-y0)**2
         rho[k]+=denval*np.exp(-dist*r0)
     return rho
-
 
 def sumcellav(rho,GlobalElementMatrix,DGorder):
     s=0
@@ -106,9 +123,23 @@ def minmod(a,b,c):
         return max(a,b,c)
     return 0
 
+def FluxLimit(rho,DGorder):
+    if DGorder>3:
+        for k in range(int(len(rho)/DGorder)):
+            rho[DGorder*k+1]=max(-3*rho[DGorder*k],min(3*rho[DGorder*k],rho[DGorder*k+1]))
+            rho[DGorder*k+2]=max(-3*rho[DGorder*k],min(3*rho[DGorder*k],rho[DGorder*k+2]))
+    return rho
+
 
 def CenterDist(k1,k2,GlobalElementMatrix,NodeList):
     return np.sqrt((NodeList[1,GlobalElementMatrix[k1,9]]-NodeList[1,GlobalElementMatrix[k2,9]])**2+(NodeList[2,GlobalElementMatrix[k1,9]]-NodeList[2,GlobalElementMatrix[k2,9]])**2)
+
+def TorX(N_row,Connectivity):
+    for k in range(N_row):
+        Connectivity[k,3]=N_row*(N_col-1)+k
+        Connectivity[N_row*(N_col-1)+k,1]=k
+    return Connectivity
+
 
 new_mesh=True
 save_mesh=False
@@ -122,10 +153,10 @@ X=XDom*(np.array([0, 1, 1, 0, 0.5 , 1   ,0.5 ,0   ,0.5],dtype=np.float64)-0.5)
 Y=YDom*np.array([0, 0, 1, 1, 0   , 0.5 ,1   ,0.5 ,0.5],dtype=np.float64)
 
 
-N_row=15
+N_row=5
 N_col=15
 
-DGorder=3
+DGorder=1
 
 
 # =============================================================================
@@ -152,7 +183,7 @@ XY=np.array([X,Y])
 
 
 if new_mesh:
-    GlobalStiffMat,GlobalForceMat,GlobalElementMatrix,NodeList,BCnodes,EleFluxMatx,EleFluxMaty,InvMassMat,b_mast_1x,b_mast_1y,b_mast_2x,b_mast_2y,b_mast_3x,b_mast_3y,b_mast_4x,b_mast_4y,Connectivity=PreProc.PreProc(XY,N_row,N_col,DGorder,5)    
+    GlobalStiffMat,GlobalForceMat,GlobalElementMatrix,NodeList,BCnodes,EleFluxMatx,EleFluxMaty,InvMassMat,b_mast_1x,b_mast_1y,b_mast_2x,b_mast_2y,b_mast_3x,b_mast_3y,b_mast_4x,b_mast_4y,LF_mast_1x,LF_mast_1y,LF_mast_2x,LF_mast_2y,LF_mast_3x,LF_mast_3y,LF_mast_4x,LF_mast_4y,Connectivity,EleVol=PreProc.PreProc(XY,N_row,N_col,DGorder,5)    
 if save_mesh:
     Mesh=GlobalStiffMat,GlobalForceMat,GlobalElementMatrix,NodeList,BCnodes,EleFluxMatx,EleFluxMaty,InvMassMat,b_mast_1x,b_mast_1y,b_mast_2x,b_mast_2y,b_mast_3x,b_mast_3y,b_mast_4x,b_mast_4y,Connectivity
     pickle("C:\\py\\Numerical Algorithms for Scientific Computing\\Meshs\\M_5050QuartCircQuadNEW",Mesh)
@@ -166,10 +197,9 @@ if load_mesh:
     GlobalStiffMat,GlobalForceMat,GlobalElementMatrix,NodeList,BCnodes,EleFluxMatx,EleFluxMaty,InvMassMat,b_mast_1x,b_mast_1y,b_mast_2x,b_mast_2y,b_mast_3x,b_mast_3y,b_mast_4x,b_mast_4y,Connectivity=LoadedMesh
     
 
-Proj=np.zeros((9,DGorder))
-for i in range(9):
-    for j in range(DGorder):
-            Proj[i,j]=(QBF.Na[i]*QBF.Ma[j]).intx(-1,1).inty(-1,1).scalar()    
+
+#pretzel x
+Connectivity=TorX(N_row,Connectivity)
 
 
 
@@ -195,8 +225,7 @@ for i in range(GlobalElementMatrix.shape[0]):
 # =============================================================================
 
 
-
-
+rho=projectFEintoDG(setInitDenDistFE(0,5,0.5,1,np.zeros(NodeList.shape[1]),NodeList,GlobalElementMatrix),NodeList,GlobalElementMatrix,DGorder)
 # =============================================================================
 # for i in range(rho.shape[0]):
 #     for j in range(rho.shape[1]-1):
@@ -206,18 +235,20 @@ for i in range(GlobalElementMatrix.shape[0]):
 #rho,circ1=setInitDenDist(0.625,0.5,0.2,1,rho,NodeList,GlobalElementMatrix)
 #rho,circ2=setInitDenDist(1.325,0.5,0.2,1,rho,NodeList,GlobalElementMatrix)
 
-
-rho,circ1=setInitDenDist(0,5,1,1,rho,NodeList,GlobalElementMatrix,DGorder)
-#rho,circ2=setInitDenDist(0.8,2,0.3,1,rho,NodeList,GlobalElementMatrix)
-#rho,circ3=setInitDenDist(-0.8,0.6,0.3,2,rho,NodeList,GlobalElementMatrix)
-#rho,circ4=setInitDenDist(0.8,0.6,0.3,-2,rho,NodeList,GlobalElementMatrix)
-
-
-plt.figure(1)
-plt.plot(NodeList[1,:],NodeList[2,:],'.')
-ax = plt.gca()
-ax.add_artist(circ1)
-#ax.add_artist(circ2)
+# =============================================================================
+# 
+# rho,circ1=setInitDenDist(0,5,1,1,rho,NodeList,GlobalElementMatrix,DGorder)
+# #rho,circ2=setInitDenDist(0.8,2,0.3,1,rho,NodeList,GlobalElementMatrix)
+# #rho,circ3=setInitDenDist(-0.8,0.6,0.3,2,rho,NodeList,GlobalElementMatrix)
+# #rho,circ4=setInitDenDist(0.8,0.6,0.3,-2,rho,NodeList,GlobalElementMatrix)
+# 
+# 
+# plt.figure(1)
+# plt.plot(NodeList[1,:],NodeList[2,:],'.')
+# ax = plt.gca()
+# ax.add_artist(circ1)
+# #ax.add_artist(circ2)
+# =============================================================================
 #ax.add_artist(circ3)
 #ax.add_artist(circ4)
 
@@ -237,9 +268,8 @@ for i in BCnodes:
     plt.plot(NodeList[1,i],NodeList[2,i],'*')
 rho0=rho
 
-DT=0.001
 #time loop
-rhoFE=setInitDenDistFE(0,5,0.5,1,np.zeros(NodeList.shape[1])
+rhoFE=setInitDenDistFE(0,5,0.5,1,np.zeros(NodeList.shape[1]),NodeList,GlobalElementMatrix)
 
 #rhoFE=setInitDenDistFE(2,6.5,0.8,1,np.zeros(NodeList.shape[1]),NodeList,GlobalElementMatrix)
 #rhoFE=setInitDenDistFE(-2,6.5,0.8,-1,rhoFE,NodeList,GlobalElementMatrix)
@@ -264,78 +294,122 @@ ax.plot_trisurf(NodeList[1,:],NodeList[2,:],Phi,
 
 
 
+
+
 FluxMat=np.zeros((GlobalElementMatrix.shape[0]*DGorder,GlobalElementMatrix.shape[0]*DGorder))
+FluxMat1=np.zeros((GlobalElementMatrix.shape[0]*DGorder,GlobalElementMatrix.shape[0]*DGorder))
+
+FluxMat2=np.zeros((GlobalElementMatrix.shape[0]*DGorder,GlobalElementMatrix.shape[0]*DGorder))
+
+FluxMat3=np.zeros((GlobalElementMatrix.shape[0]*DGorder,GlobalElementMatrix.shape[0]*DGorder))
+
 for k in range(GlobalElementMatrix.shape[0]):
     XYele = NodeList[1:3,GlobalElementMatrix[k,1:10]]
+    alpha1=0
+    alpha2=0
+    alpha3=0
+    alpha4=0
     for i in range(DGorder):
+        alpha1+=LF_mast_1x[k,i]*vx[DGorder*k+i]-LF_mast_1y[k,i]*vy[DGorder*k+i]
+        alpha2+=LF_mast_2x[k,i]*vx[DGorder*k+i]-LF_mast_2y[k,i]*vy[DGorder*k+i]
+        alpha3+=LF_mast_3x[k,i]*vx[DGorder*k+i]-LF_mast_3y[k,i]*vy[DGorder*k+i]
+        alpha4+=LF_mast_4x[k,i]*vx[DGorder*k+i]-LF_mast_4y[k,i]*vy[DGorder*k+i]
+        
         for j in range(DGorder):    
             for m in range(DGorder):
-                FluxMat[DGorder*k+i,DGorder*k+j]+=EleFluxMatx[k,i,j,m]*vx[DGorder*k+m]+EleFluxMaty[k,i,j,m]*vy[DGorder*k+m]
+                FluxMat2[DGorder*k+i,DGorder*k+j]+=EleFluxMatx[k,i,j,m]*vx[DGorder*k+m]+EleFluxMaty[k,i,j,m]*vy[DGorder*k+m]
                 
-                FluxMat[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_1x[k,i,j,m]*vx[DGorder*k+m]-b_mast_1y[k,i,j,m]*vy[DGorder*k+m])
+                FluxMat1[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_1x[k,i,j,m]*vx[DGorder*k+m]-b_mast_1y[k,i,j,m]*vy[DGorder*k+m])
+                FluxMat1[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_2x[k,i,j,m]*vx[DGorder*k+m]-b_mast_2y[k,i,j,m]*vy[DGorder*k+m])
+                FluxMat1[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_3x[k,i,j,m]*vx[DGorder*k+m]-b_mast_3y[k,i,j,m]*vy[DGorder*k+m])
+                FluxMat1[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_4x[k,i,j,m]*vx[DGorder*k+m]-b_mast_4y[k,i,j,m]*vy[DGorder*k+m])
                 
-                FluxMat[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_2x[k,i,j,m]*vx[DGorder*k+m]-b_mast_2y[k,i,j,m]*vy[DGorder*k+m])
-                FluxMat[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_3x[k,i,j,m]*vx[DGorder*k+m]-b_mast_3y[k,i,j,m]*vy[DGorder*k+m])
-                FluxMat[DGorder*k+i,DGorder*k+j]+=0.5*(b_mast_4x[k,i,j,m]*vx[DGorder*k+m]-b_mast_4y[k,i,j,m]*vy[DGorder*k+m])
+
+
                 
                 if Connectivity[k,0]!=-1:
-                    FluxMat[DGorder*Connectivity[k,0]+i,DGorder*k+j]-=0.5*(b_mast_3x[k,i,j,m]*vx[DGorder*k+m]-b_mast_3y[k,i,j,m]*vy[DGorder*k+m])
+                    FluxMat1[DGorder*Connectivity[k,0]+i,DGorder*k+j]-=0.5*(b_mast_3x[k,i,j,m]*vx[DGorder*k+m]-b_mast_3y[k,i,j,m]*vy[DGorder*k+m])
                         
                 if Connectivity[k,1]!=-1:
-                    FluxMat[DGorder*Connectivity[k,1]+i,DGorder*k+j]-=0.5*(b_mast_4x[k,i,j,m]*vx[DGorder*k+m]-b_mast_4y[k,i,j,m]*vy[DGorder*k+m])
+                    FluxMat1[DGorder*Connectivity[k,1]+i,DGorder*k+j]-=0.5*(b_mast_4x[k,i,j,m]*vx[DGorder*k+m]-b_mast_4y[k,i,j,m]*vy[DGorder*k+m])
                         
                 if Connectivity[k,2]!=-1:
-                    FluxMat[DGorder*Connectivity[k,2]+i,DGorder*k+j]-=0.5*(b_mast_1x[k,i,j,m]*vx[DGorder*k+m]-b_mast_1y[k,i,j,m]*vy[DGorder*k+m])
+                    FluxMat1[DGorder*Connectivity[k,2]+i,DGorder*k+j]-=0.5*(b_mast_1x[k,i,j,m]*vx[DGorder*k+m]-b_mast_1y[k,i,j,m]*vy[DGorder*k+m])
                 
                 if Connectivity[k,3]!=-1: 
-                    FluxMat[DGorder*Connectivity[k,3]+i,DGorder*k+j]-=0.5*(b_mast_2x[k,i,j,m]*vx[DGorder*k+m]-b_mast_2y[k,i,j,m]*vy[DGorder*k+m])
+                    FluxMat1[DGorder*Connectivity[k,3]+i,DGorder*k+j]-=0.5*(b_mast_2x[k,i,j,m]*vx[DGorder*k+m]-b_mast_2y[k,i,j,m]*vy[DGorder*k+m])
+        
+    alpha1=abs(alpha1)
+    alpha2=abs(alpha2)
+    alpha3=abs(alpha3)        
+    alpha4=abs(alpha4)      
+    
+    FluxMat3[DGorder*k,DGorder*k]+=0.5*(alpha1)
+    FluxMat3[DGorder*k,DGorder*k]+=0.5*(alpha2)
+    FluxMat3[DGorder*k,DGorder*k]+=0.5*(alpha3)
+    FluxMat3[DGorder*k,DGorder*k]+=0.5*(alpha4)
+            
 
-                                           
 
-for i in range(50):
+            
+    if Connectivity[k,0]!=-1:
+        FluxMat3[DGorder*Connectivity[k,0],DGorder*k]-=0.5*(alpha3)
+        
+    if Connectivity[k,1]!=-1:
+        FluxMat3[DGorder*Connectivity[k,1],DGorder*k]-=0.5*(alpha4)
+        
+    if Connectivity[k,2]!=-1:
+        FluxMat3[DGorder*Connectivity[k,2],DGorder*k]-=0.5*(alpha1)
+            
+    if Connectivity[k,3]!=-1: 
+        FluxMat3[DGorder*Connectivity[k,3],DGorder*k]-=0.5*(alpha2)
+                                            
+plt.figure(15)
+plt.imshow(FluxMat1)
 
-    rho1=rho+DT*InvMassMat@FluxMat@rho
-# =============================================================================
-#     
-#     for k in range(GlobalElementMatrix.shape[0]):
-#         if Connectivity[k,1]!=-1 and Connectivity[k,3]!=-1:
-#             rho1[9*k+1]=minmod(rho1[9*k+1],(rho1[9*Connectivity[k,1]]-rho1[9*k])/CenterDist(k,Connectivity[k,1],GlobalElementMatrix,NodeList),(rho1[9*k]-rho1[9*Connectivity[k,3]])/CenterDist(k,Connectivity[k,3],GlobalElementMatrix,NodeList))
-#         if Connectivity[k,0]!=-1 and Connectivity[k,2]!=-1:
-#             rho1[9*k+2]=minmod(rho1[9*k+2],(rho1[9*Connectivity[k,2]]-rho1[9*k])/CenterDist(k,Connectivity[k,2],GlobalElementMatrix,NodeList),(rho1[9*k]-rho1[9*Connectivity[k,0]])/CenterDist(k,Connectivity[k,0],GlobalElementMatrix,NodeList))
-#     
-# =============================================================================
+plt.figure(16)
+plt.imshow(FluxMat2)
+
+plt.figure(17)
+plt.imshow(FluxMat3)
+
+FluxMat=FluxMat1+FluxMat2+FluxMat3
+
+plt.figure(18)
+plt.imshow(FluxMat)
+
+
+DT=0.1
+
+for i in range(100):
+
+    CFL=DT*np.maximum(FluxMat,0)@np.ones(rho.shape)/EleVol
+    rho_f1=np.maximum(np.minimum(rho,rho/(4*CFL)),0)
+    
+    rho1=rho+DT*InvMassMat@(FluxMat@rho_f1)
+    rho1=FluxLimit(rho1,DGorder) 
+    
+    rho_f2=np.maximum(np.minimum(rho1,rho1/(4*CFL)),0)
     
     
-    rho2=0.75*rho+0.25*rho1+0.25*DT*InvMassMat@FluxMat@rho1
+    rho2=0.75*rho+0.25*rho1+0.25*DT*InvMassMat@(FluxMat@rho_f2)
+    rho2=FluxLimit(rho2,DGorder)
+
+    rho_f3=np.maximum(np.minimum(rho2,rho2/(4*CFL)),0)
+
+
+    rho3=1/3*rho+2/3*rho2+2/3*DT*InvMassMat@(FluxMat@rho_f3)
+    rho3=FluxLimit(rho3,DGorder)
+
 
 # =============================================================================
 #     for k in range(GlobalElementMatrix.shape[0]):
-#         if Connectivity[k,1]!=-1 and Connectivity[k,3]!=-1:
-#             rho2[9*k+1]=minmod(rho2[9*k+1],(rho2[9*Connectivity[k,1]]-rho2[9*k])/CenterDist(k,Connectivity[k,1],GlobalElementMatrix,NodeList),(rho2[9*k]-rho2[9*Connectivity[k,3]])/CenterDist(k,Connectivity[k,3],GlobalElementMatrix,NodeList))
-#         if Connectivity[k,0]!=-1 and Connectivity[k,2]!=-1:
-#             rho2[9*k+2]=minmod(rho2[9*k+2],(rho2[9*Connectivity[k,2]]-rho2[9*k])/CenterDist(k,Connectivity[k,2],GlobalElementMatrix,NodeList),(rho2[9*k]-rho2[9*Connectivity[k,0]])/CenterDist(k,Connectivity[k,0],GlobalElementMatrix,NodeList))
-# 
-# 
+#         if Connectivity[k,1]==-1 or Connectivity[k,3]==-1 or Connectivity[k,0]==-1 or Connectivity[k,2]==-1:
+#             rho3[DGorder*k:DGorder*(k+1)]=rho0[DGorder*k:DGorder*(k+1)]
 # =============================================================================
-
-    rho3=1/3*rho+2/3*rho2+2/3*DT*InvMassMat@FluxMat@rho2
-
-    
-# =============================================================================
-#     for k in range(GlobalElementMatrix.shape[0]):
-#         if Connectivity[k,1]!=-1 and Connectivity[k,3]!=-1:
-#             rho3[9*k+1]=minmod(rho3[9*k+1],(rho3[9*Connectivity[k,1]]-rho3[9*k])/CenterDist(k,Connectivity[k,1],GlobalElementMatrix,NodeList),(rho2[9*k]-rho3[9*Connectivity[k,3]])/CenterDist(k,Connectivity[k,3],GlobalElementMatrix,NodeList))
-#         if Connectivity[k,0]!=-1 and Connectivity[k,2]!=-1:
-#             rho3[9*k+2]=minmod(rho3[9*k+2],(rho3[9*Connectivity[k,2]]-rho3[9*k])/CenterDist(k,Connectivity[k,2],GlobalElementMatrix,NodeList),(rho2[9*k]-rho3[9*Connectivity[k,0]])/CenterDist(k,Connectivity[k,0],GlobalElementMatrix,NodeList))
-# 
-# =============================================================================
-    for k in range(GlobalElementMatrix.shape[0]):
-        if Connectivity[k,1]==-1 or Connectivity[k,3]==-1 or Connectivity[k,0]==-1 or Connectivity[k,2]==-1:
-            rho3[DGorder*k:DGorder*(k+1)]=rho0[DGorder*k:DGorder*(k+1)]
             
     rho=rho3
 
-#plt.imshow(FluxMat)
 
 
 
@@ -343,9 +417,12 @@ for i in range(50):
 
 #plt.imshow(GlobalStiffMat)
 
-plotDGelem(rho, GlobalElementMatrix, NodeList, 5, DGorder)
-plotDGelem(rho0, GlobalElementMatrix, NodeList, 6, DGorder)
+plotDGelem(rho, GlobalElementMatrix, NodeList, 5, DGorder,3)
+plotDGelem(rho0, GlobalElementMatrix, NodeList, 6, DGorder,3)
 
 #200 timesteps
+print('rho0 = ' +str(sumcellav(rho0,GlobalElementMatrix,DGorder))+'\nrho = ' +str(sumcellav(rho,GlobalElementMatrix,DGorder))+' \nDelta = '+str(sumcellav(rho0,GlobalElementMatrix,DGorder)-sumcellav(rho,GlobalElementMatrix,DGorder)))
+
+
 
 
